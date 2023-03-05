a19e34f023dc6c1df11a5fbc39a1880cf82c7a37
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-03-05 17:24:20 +0530

    addSong function null Pointer error corrected

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -6,6 +6,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+//		SpotifyController controller = new SpotifyController();
+//		controller.createUser("Rahul", "11223344");
+//		controller.createUser("Rohit", "12341234");
+//		controller.createArtist("Arijit Singh");
+//		controller.createArtist("Lata Ji");
+//		controller.createAlbum("Tere Bin", "Arijit Singh");
+//		controller.createAlbum("Old Songs", "Lata Ji");
+//		try {
+//			controller.createSong("Mai Jo Hu", "Tere Bin", 12);
+//		} catch (Exception e) {
+//			System.out.println("Exception found in create song"+e);
+//		}
+//		try {
+//
+//		controller.createSong("Tumne Jo Kha", "Old Songs", 13);
+//	    }
+//		catch (Exception e) {
+//			System.out.println("Exception found in create song"+e);
+//		}
+//		try{	controller.likeSong("11223344", "Mai Jo Hu");
+//		}
+//		catch (Exception e){
+//			System.out.println("Exception found in like song"+e);
+//		}
+//		System.out.println(controller.mostPopularArtist());
+//		System.out.println(controller.mostPopularSong());
+
+
+
 	}
 }
--- src/main/java/com/driver/SpotifyRepository.java
+++ src/main/java/com/driver/SpotifyRepository.java
@@ -6,21 +6,21 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class SpotifyRepository {
-    public static HashMap<Artist, List<Album>> artistAlbumMap;
-    public static HashMap<Album, List<Song>> albumSongMap;
-    public  static HashMap<Playlist, List<Song>> playlistSongMap;
-    public static HashMap<Playlist, List<User>> playlistListenerMap;
-    public static HashMap<User, Playlist> creatorPlaylistMap;
-    public static HashMap<User, List<Playlist>> userPlaylistMap;
-    public static HashMap<Song, List<User>> songLikeMap;
+    public HashMap<Artist, List<Album>> artistAlbumMap;
+    public HashMap<Album, List<Song>> albumSongMap;
+    public HashMap<Playlist, List<Song>> playlistSongMap;
+    public HashMap<Playlist, List<User>> playlistListenerMap;
+    public HashMap<User, Playlist> creatorPlaylistMap;
+    public HashMap<User, List<Playlist>> userPlaylistMap;
+    public HashMap<Song, List<User>> songLikeMap;
 
-    public static List<User> users;
-    public static List<Song> songs;
-    public static List<Playlist> playlists;
-    public static List<Album> albums;
-    public static List<Artist> artists;
+    public List<User> users;
+    public List<Song> songs;
+    public List<Playlist> playlists;
+    public List<Album> albums;
+    public List<Artist> artists;
 
-    public static HashMap<User,List<Song>> userLikedSongsMap;
+    public HashMap<User,List<Song>> userLikedSongsMap;
 
     public SpotifyRepository(){
         //To avoid hitting apis multiple times, initialize all the hashmaps here with some dummy data
@@ -94,7 +94,11 @@ public class SpotifyRepository {
         }
 
         Song newSong = new Song(title,length);
-         List<Song> songList = albumSongMap.get(currentAlbum);
+
+         List<Song> songList = new ArrayList<>();
+         if(albumSongMap.containsKey(albumName)){
+             songList = albumSongMap.get(albumName);
+         }
          songList.add(newSong);
          albumSongMap.put(currentAlbum,songList);
          songs.add(newSong);
